85d9aed3a581001212cf0acb57c38f5004368a6f
David <d.c.reinartz@gmail.com>
2020-09-29 22:15:22 +0200

    Add basic Ktor project with Mustache and GSON

---
+++ src/Application.kt
@@ -0,0 +1,44 @@
+package io.doodlebot
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import com.github.mustachejava.DefaultMustacheFactory
+import io.ktor.mustache.Mustache
+import io.ktor.mustache.MustacheContent
+import io.ktor.gson.*
+import io.ktor.features.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Mustache) {
+        mustacheFactory = DefaultMustacheFactory("templates/mustache")
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/html-mustache") {
+            call.respond(MustacheContent("index.hbs", mapOf("user" to MustacheUser(1, "user1"))))
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+data class MustacheUser(val id: Int, val name: String)
+
