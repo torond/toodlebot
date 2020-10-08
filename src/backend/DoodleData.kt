8bd2bc21eea3771ed47cb5722f5c8c8c60d5ff9b
David <d.c.reinartz@gmail.com>
2020-10-08 16:24:56 +0200

    Remove pickeDates and pickableDates fields from DoodleData.kt

--- src/backend/DoodleData.kt
+++ src/backend/DoodleData.kt
@@ -3,7 +3,5 @@ package io.doodlebot.backend
 import java.time.LocalDate
 
 data class DoodleData(
-        val pickedDates: List<LocalDate>,
-        val pickableDates: List<LocalDate>,
         val pickableDatesStatus: List<Pair<LocalDate, List<String>>>,
         val numberOfParticipants: Int)
