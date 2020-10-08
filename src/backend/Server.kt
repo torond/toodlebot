14618b2c35f8a2d898cf6072dd8208d4675e66f1
David <d.c.reinartz@gmail.com>
2020-10-08 14:01:05 +0200

    Add and rename endpoints

--- src/backend/Server.kt
+++ src/backend/Server.kt
@@ -28,17 +28,44 @@ fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) { gson {} }
 
     routing {
-        /** Setup and Admin Edit Endpoint */
+        /** Endpoint for setting up and editing the initial dates of a Doodle */
         get("/setup") {
             // TODO: Generate Crypto URL and redirect
             call.respond(MustacheContent("frontend.hbs", mapOf("config" to DoodleConfig.SETUP)))
         }
 
-        post("/genDoodle") {
+        /** Accepts setup dates for the Doodle */
+        post("/setup") {
             val pickedDates: List<LocalDate> = call.receive()
-            println(pickedDates)
             // TODO: Persist dates
             call.respond(HttpStatusCode.OK)
+        }
+
+        /** Endpoint for answering and editing the answers of a Doodle */
+        get("/edit") {
+            // TODO: Accept Crypto URL
+            // TODO: Authentication / recognize user
+            call.respond(MustacheContent("frontend.hbs", mapOf("config" to DoodleConfig.EDIT)))
+        }
+
+        /** Accepts edits to the answers of a Doodle */
+        post("/edit") {
+            call.respond(HttpStatusCode.OK)
+        }
+
+        /** Endpoint for closing a Doodle */
+        get("/close") {
+            call.respond(MustacheContent("frontend.hbs", mapOf("config" to DoodleConfig.CLOSE)))
+        }
+
+        /** Accepts final dates of a Doodle*/
+        post("/close") {
+            call.respond(HttpStatusCode.OK)
+        }
+
+        /** Endpoint for viewing the results of a Doodle*/
+        get("/view") {
+            call.respond(MustacheContent("frontend.hbs", mapOf("config" to DoodleConfig.VIEW)))
         }
     }
 }
