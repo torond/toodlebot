d07ed0fe4e72bebfc36af1c1be2fcc8c6b0187b6
David <d.c.reinartz@gmail.com>
2020-10-08 14:01:05 +0200

    Add and rename endpoints

--- src/backend/DoodleConfig.kt
+++ src/backend/DoodleConfig.kt
@@ -1,12 +1,12 @@
 package io.doodlebot.backend
 
 enum class DoodleConfig(
-        val text: String,
+        val description: String,
         val hasConfirmButton: Boolean,
-        val isEditable: Boolean,
-        val confirmUrl: String) {
-    SETUP("Pick dates to add to the Doodle.", true, true, "/genDoodle"),
-    EDIT("Choose dates you can attend to.", true, true, "/todo"),
-    CLOSING("Select final dates.", true, true, "/todo"),
-    RESULTS("These dates have been selected.", false, false, "/todo")
+        val calendarIsEditable: Boolean,
+        val confirmRecipientUrl: String) {
+    SETUP("Pick dates to add to the Doodle.", true, true, "/setup"),
+    EDIT("Choose dates you can attend to.", true, true, "/edit"),
+    CLOSE("Select final dates.", true, true, "/close"),
+    VIEW("These dates have been selected.", false, false, "/view")
 }
