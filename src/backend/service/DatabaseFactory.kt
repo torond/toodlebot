7b5d475949c5ed7a9162422d9b6eaed0ecfae061
David <d.c.reinartz@gmail.com>
2021-04-22 15:38:33 +0200

    Implement editing of answers and authentication of participants

--- src/backend/service/DatabaseFactory.kt
+++ src/backend/service/DatabaseFactory.kt
@@ -1,19 +1,22 @@
 package io.doodlebot.backend.service
 
-import io.doodlebot.backend.model.DoodleDates
-import io.doodlebot.backend.model.DoodleInfos
-import io.doodlebot.backend.model.InfoJoinDate
-import io.doodlebot.backend.model.Participations
+import io.doodlebot.backend.model.*
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.SchemaUtils.create
 import org.jetbrains.exposed.sql.SchemaUtils.drop
+import org.jetbrains.exposed.sql.insertAndGetId
 import java.sql.Connection
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 object DatabaseFactory {
-    fun init() {
+    val dummyDateId: EntityID<Int>
+    init {
+        println("Initializin database")
         Database.connect("jdbc:sqlite:./data/data.db", "org.sqlite.JDBC")
         TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
         transaction {
@@ -21,13 +24,18 @@ object DatabaseFactory {
             drop(DoodleDates)
             drop(InfoJoinDate)
             drop(Participations)
+            drop(Participants)
             create(DoodleInfos)
             create(DoodleDates)
             create(InfoJoinDate)
             create(Participations)
+            create(Participants)
+        }
+        // Insert dummy value for marking a successful answer to a Doodle
+        dummyDateId = transaction {
+            DoodleDates.insertAndGetId { it[doodleDate] = LocalDate.parse("0001-01-01", DateTimeFormatter.ofPattern("yyyy-MM-dd")) }
         }
     }
-
 
     suspend fun <T> dbQuery(block: suspend () -> T): T = newSuspendedTransaction { block() }
 }
