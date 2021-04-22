d40ba7c27689ecc38a0a5b22e3d8369b66595ed0
David <d.c.reinartz@gmail.com>
2021-04-22 15:43:27 +0200

    Move comments from code to doc

--- src/backend/Server.kt
+++ src/backend/Server.kt
@@ -108,42 +108,6 @@ fun Application.module(testing: Boolean = false) {
         this.application.environment.log.debug("${this.request.httpMethod.value} to ${this.request.path()} by $loginData")
     }
 
-
-    //pickableDates by doodleId (dates enabled in the calendar)
-    // -> /setup/{doodleId}: All dates
-    // -> /answer/{doodleId}: Dates chosen by admin on setup -> proposedDates
-    // -> /close/{doodleId}: Dates chosen by admin on setup -> proposedDates
-    // -> /view/{doodleId}: none
-    //pickedDates by doodleId & participantId (prev. picked dates for updating doodle or answers)
-    // -> /setup/{doodleId}: Dates prev. chosen by admin -> proposedDates
-    // -> /answer/{doodleId}: Dates prev. chosen by participant -> yesDates
-    //finalDates by doodleId
-    // -> /close/{doodleId}: Final dates prev. chosen by admin -> finalDates
-    // -> /view/{doodleId}: Final dates prev. chosen by admin -> finalDates
-    //
-    // => Mustache Template anpassen, s.d. Server.kt verständlich bleibt
-
-    // MAPPING
-    // (0. All dates / no dates -> controlled by DoodleConfig or absence of 1.)
-    // 1. Dates chosen by admin on setup, open for answers -> proposedDates in backend, enabledDates or defaultDates in frontend
-    // 2. Dates chosen by participants -> yesDates in backend, defaultDates in frontend (other: chosenDates, answeredDates, repliedDates, respondedDates, consideredDates, committedDates, attendableDates)
-    // 3. Final dates chosen by admin -> finalDates in backend and frontend
-
-    /*
-    * Principle of least astonishment.
-    * Be precise.
-    * -> Mention parameters (byDoodleId), but not in add methods?
-    * -> ID's always with corresponding Table (DoodleId not Uuid)
-    *
-    * -> No EntityId<...> in Server.kt, at most the doodle doodleId
-    * -> No other IDs? Yes.
-    *
-    * ideas:
-    * - only update methods -> addOrUpdate
-    *
-    * questions: Optional return types?
-    * */
-
     fun buildMustacheMapping(config: DoodleConfig,
                              enabledDates: List<LocalDate>? = null,
                              defaultDates: List<LocalDate>? = null,
