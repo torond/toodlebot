b2e5c004f87395441f3322694560f36eca315bc9
David <d.c.reinartz@gmail.com>
2021-04-30 20:09:07 +0200

    Fix /delete not working, remove unwanted println() statements

--- src/Application.kt
+++ src/Application.kt
@@ -8,9 +8,6 @@ import kotlin.concurrent.thread
 
 
 fun main() {
-    /*thread(start=true) {
-        bot.start()
-    }*/
     Env  // To make sure that Env is initialized before anything else, e.g. HashUtil
     thread(start=true) {
         embeddedServer(Netty, host="0.0.0.0", port = Env.port.toInt(), module = Application::module).start(wait = true)
